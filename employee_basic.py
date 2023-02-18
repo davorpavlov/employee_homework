c6aba363f47ab95ee4d89476299ad8ec88620063
davorpavlov <pavlovic.davor91@gmail.com>
2023-02-18 20:27:09 +0100

    Create employee_basic.py

---
+++ employee_basic.py
@@ -0,0 +1,45 @@
+# Data of the first employee
+name1 = 'Goran'
+surname1 = 'Goric'
+position1 = 'Accountant'
+salary1 = 1500
+email1 = 'goran.goric@firma.com'
+phone_number1 = '0981234567'
+
+# Data of the second employee
+name2 = 'Darko'
+surname2 = 'Darkovic'
+position2 = 'Developer'
+salary2 = 2000
+email2 = 'darko.darkovic@firma.com'
+phone_number2 = '0919876543'
+
+# Data of the third employee
+name3 = 'Mauro'
+surname3 = 'Maurovic'
+position3 = 'CEO'
+salary3 = 3000
+email3 = 'mauro.maurovic@firma.com'
+phone_number3 = '095225883'
+
+# Printing the data of each employee
+print('Name and surname:', name1, surname1)
+print('Position:', position1)
+print('Salary:', salary1)
+print('Email:', email1)
+print('Phone number:', phone_number1)
+print()
+
+print('Name and surname:', name2, surname2)
+print('Position:', position2)
+print('Salary:', salary2)
+print('Email:', email2)
+print('Phone number:', phone_number2)
+print()
+
+print('Name and surname:', name3, surname3)
+print('Position:', position3)
+print('Salary:', salary3)
+print('Email:', email3)
+print('Phone number:', phone_number3)
+print()
